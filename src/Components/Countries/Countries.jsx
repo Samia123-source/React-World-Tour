ab0c08cc63a7ed6c7e44109bcb06cd75db094c21
Samia123-source <samiamahim04@gmail.com>
2026-03-03 00:08:18 +0600

    toggle state based on click and conditional redering

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css';
 
 
 const Countries = () => {
     const[countries, setCountries] = useState([]);
     useEffect (() => {
-        fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,area')
+        fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,cca2,cca3')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
@@ -13,11 +14,14 @@ const Countries = () => {
     return (
         <div>
             <h3>Total countries = {countries.length}</h3>
-        {
-           countries.map(country => <Country key={country.name.official} country={country}>
+            <div className="country-countainer">
+                  {
+           countries.map(country => <Country key={country.cca3} country={country}>
 
            </Country>)
         }
+            </div>
+      
            {/* {
              countries.map(country => <Country key={country.name.official} country={country}></Country>)
            } */}
