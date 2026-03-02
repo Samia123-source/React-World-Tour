10e33124092e23d2f8dbca211b4a5cfff389b4a4
Samia123-source <samiamahim04@gmail.com>
2026-03-02 15:03:06 +0600

    recap folder structure

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import Country from "./Country/Country";
+import Country from "../Country/Country";
+
 
 const Countries = () => {
     const[countries, setCountries] = useState([]);
     useEffect (() => {
-        fetch('https://restcountries.com/v3.1/all?fields=name,flags,co')
+        fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,area')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
@@ -12,9 +13,14 @@ const Countries = () => {
     return (
         <div>
             <h3>Total countries = {countries.length}</h3>
-           {
+        {
+           countries.map(country => <Country key={country.name.official} country={country}>
+
+           </Country>)
+        }
+           {/* {
              countries.map(country => <Country key={country.name.official} country={country}></Country>)
-           }
+           } */}
          
             
         </div>
