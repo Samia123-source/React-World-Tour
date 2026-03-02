808bf0fd4f2d3b01bf2fa52f70311314c47d2e92
Samia123-source <samiamahim04@gmail.com>
2026-03-03 01:45:48 +0600

    conditional css and conditional style

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -5,18 +5,34 @@ import './Countries.css';
 
 const Countries = () => {
     const[countries, setCountries] = useState([]);
+     const[visitedCountries, setVisitedCountries] = useState([]);
+   
+
     useEffect (() => {
         fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,cca2,cca3')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
+   
+    const handleVisitedCountry = country => 
+    {console.log('add this to your visited country');
+        console.log(country);
+    }
 
-    return (
+    return(
         <div>
             <h3>Total countries = {countries.length}</h3>
+        <div>
+                <h5>Visited countries</h5>
+                <ul>
+
+                </ul>
+        </div>
             <div className="country-countainer">
                   {
-           countries.map(country => <Country key={country.cca3} country={country}>
+           countries.map(country => <Country key={country.cca3}
+           handleVisitedCountry={handleVisitedCountry}
+            country={country}>
 
            </Country>)
         }
